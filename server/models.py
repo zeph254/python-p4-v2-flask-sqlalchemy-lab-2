f418ce4d2d02059438d05efcb773c071e80dff5a
zeph254 <ularezephaniah@gmail.com>
2025-01-21 18:42:55 +0300

    migrations

--- server/models.py
+++ server/models.py
@@ -17,6 +17,8 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
@@ -28,5 +30,21 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+         
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}, {self.customer_id}, {self.item_id}>'
